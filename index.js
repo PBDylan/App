ed9ffaa82993e5873d47c8f01ac6c5ec35f1753c
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-09-09 22:58:20 -0300

    fundamentos

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ console.log(mensagem_2);
 
 //Sobre escopos, {} se trata de escopos diferentes, dentro de {} é local, enquanto o que está fora é global. Ele vem antes de global se tiver uma chamada "console.log" por exemplo.
 
+{
+    const mensagem_2 = "Hy Pereira!"
+    console.log(mensagem_2)
+}
+
+//Arrays, objetos
+//Listas ordenas e/ou organizadas são arrays
+//["indice_0", "indice_1"]
+let metas = ["dylan", "alo"]
+
+//Os + são para concatenação
+console.log(metas[1] + ", " + metas[0])
+
+//Objeto = {}
+let meta = {
+    value: 'ler um livro todo mês',
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+meta.log(meta.value)
+
+//Function // arrow function
+//const criarMeta = () => {}
+
+//function criarMeta() {}
