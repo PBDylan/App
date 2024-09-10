e5a45bcf1e214d31878783bcd63fd5c9e94c117c
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-09-10 16:51:28 -0300

    menu da aplicação e fundamentos

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+//require devolve um objeto, que será o select. Estamos importando módulos/bibliotecas.
+const { select } = require('@inquirer/prompts')
+
 //Ou function start() {}
-const start = () => {
+//Sempre que você tiver "await" na func, vai precisarescreve assim a const: const async start
+//await significa que o seu código está esperando que uma function "select" cumpra a promessa de trazer uma resposta positiva ou negativa em relação ao pedido do cliente.
+const start = async () => {
     while(true){
-        let opcao = "sair"
+        
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "Cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
         switch(opcao) {
             case "cadastrar":
                 console.log("vamos cadastrar")
@@ -10,6 +33,7 @@ const start = () => {
                 console.log("vamos listar")
                 break
             case "sair":
+                console.log("Até a próxima!")
                 return
         }
     }
