e0ec3c9c223969c8036c7e3bad2db2297d4c37ac
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-09-10 17:31:51 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 //require devolve um objeto, que será o select. Estamos importando módulos/bibliotecas.
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
 
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+//length significa número de caracteres
+    if(meta.length == 0) {
+        console.log('A meta não pode ser vazia.')
+        return
+    }
+
+    metas.push(
+        { value: meta, checked: false }
+    )
+
+}
 //Ou function start() {}
 //Sempre que você tiver "await" na func, vai precisarescreve assim a const: const async start
 //await significa que o seu código está esperando que uma function "select" cumpra a promessa de trazer uma resposta positiva ou negativa em relação ao pedido do cliente.
@@ -26,11 +47,12 @@ const start = async () => {
         })
 
         switch(opcao) {
-            case "cadastrar":
-                console.log("vamos cadastrar")
+            case "Cadastrar":
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                console.log("Vamos listar")
                 break
             case "sair":
                 console.log("Até a próxima!")
