11f40dd1137aab843a6b1b51c123f8cfde0fe0ed
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-09-10 15:32:38 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
-//Variáveis
-let mensagem = "Hello Dydy!"
-let number = 2
-
-//Constante
-const mensagem_2 = "Tchau Dydy!"
-
-// hello world!
-console.log("hello world!");
-console.log(number);
-console.log(mensagem);
-console.log(mensagem_2);
-
-//Sobre escopos, {} se trata de escopos diferentes, dentro de {} é local, enquanto o que está fora é global. Ele vem antes de global se tiver uma chamada "console.log" por exemplo.
-
-{
-    const mensagem_2 = "Hy Pereira!"
-    console.log(mensagem_2)
-}
-
-//Arrays, objetos
-//Listas ordenas e/ou organizadas são arrays
-//["indice_0", "indice_1"]
-//let metas = ["dylan", "alo"]
-
-//Os + são para concatenação
-//console.log(metas[1] + ", " + metas[0])
-
-//Objeto = {}
-let meta = {
-    value: 'ler um livro todo mês',
-    checked: true,
-}
-
-let metas = [
-    meta,
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked: false
+//Ou function start() {}
+const start = () => {
+    let count = 1
+    while(count <= 10){
+        console.log(count)
+        count = count + 1
     }
-]
 
-console.log(metas[1].value)
+}
 
-//Function // arrow function
-//const criarMeta = () => {}
-
-//function criarMeta() {}
+start()
