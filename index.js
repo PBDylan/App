c34513e488e57ab32f37cb444ab95d141d329b51
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-09-12 17:03:53 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -63,8 +63,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizdas",
+        message: "Metas Realizdas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+    const metasAbertas = async () => {
+        const abertas = metas.filter((meta) => {
+            return meta.checked != true
+    })
+
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas ! :)")
+        return
+}
+
+    await select ({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -90,6 +106,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -107,6 +127,9 @@ const start = async () => {
             case "realizadas":
                 await metasRealizadas()
                 break
+            case "abertas":
+                await metasAbertas()
+                break        
             case "sair":
                 console.log("Até a próxima!")
                 return
