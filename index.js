ee9cca903af40e17991158ebc730fb0884703989
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-09-12 16:33:31 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -51,6 +51,21 @@ const listaMetas = async () => {
 
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log('Não existem metas realizadas! :(')
+        return
+    }
+
+    await select({
+        message: "Metas Realizdas",
+        choices: [...realizadas]
+    })
+}
 
 //Ou function start() {}
 //Sempre que você tiver "await" na func, vai precisarescreve assim a const: const async start
@@ -70,6 +85,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -83,6 +102,9 @@ const start = async () => {
                 break
             case "listar":
                 await listaMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima!")
