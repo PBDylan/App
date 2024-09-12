fb04746bc082f4f7527e42c9567c8d4798ed4aec
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-09-12 16:40:20 -0300

    resolve problemas marcadas/desmarcadas

--- index.js
+++ index.js
@@ -30,15 +30,16 @@ const listaMetas = async () => {
         instructions: false,
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if (respostas.length == 0) {
         console.log("Nenhuma meta selecionada!")
         return
     }
 
-    metas.forEach((m) => {
-        m.checked = false
-    })
-
+    
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
             return m.value == resposta
